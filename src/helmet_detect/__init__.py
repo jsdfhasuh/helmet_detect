ff4dfab982c33d0dd9dcfc4113cb04e713b2f972
Jiahao Cai <49879362+jsdfhasuh@users.noreply.github.com>
2026-09-03 18:17:47 +0800

    feat: expose dynamic V2 API

--- src/helmet_detect/__init__.py
+++ src/helmet_detect/__init__.py
@@ -1,15 +1,24 @@
 """Helmet detection package."""
 
 from .config import DetectorConfig, ModelConfig, TemporalConfig, load_config
+from .dynamic_config import DynamicDetectorConfig, load_dynamic_config
+from .dynamic_pipeline import DynamicHelmetDetectionPipeline
+from .dynamic_types import DynamicFrameResult, HelmetState, PersonObservation
 from .pipeline import FrameResult, HelmetDetectionPipeline
 
 __all__ = [
     "DetectorConfig",
+    "DynamicDetectorConfig",
+    "DynamicFrameResult",
+    "DynamicHelmetDetectionPipeline",
     "FrameResult",
     "HelmetDetectionPipeline",
+    "HelmetState",
     "ModelConfig",
+    "PersonObservation",
     "TemporalConfig",
     "load_config",
+    "load_dynamic_config",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
